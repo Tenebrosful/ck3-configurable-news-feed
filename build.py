5631e0f39f14396b6829729318343f288711da26
dungeonyak <dungeonyak@fastmail.com>
2021-11-07 10:48:22 -0500

    Create script to generate placeholder loc strings for other languages based on English loc

---
+++ build.py
@@ -0,0 +1,49 @@
+import os
+import pathlib
+import yaml
+from yaml import CLoader as Loader, CDumper as Dumper
+
+non_english_languages = [
+    'french',
+    'german',
+    'korean',
+    'russian',
+    'simp_chinese',
+    'spanish',
+]
+
+def get_all_localization(language):
+    localization = {}
+
+    for root, dirs, files in os.walk(os.path.join('.', 'localization', language)):
+        for filename in files:
+            if filename == f"newsfeed_generated_l_{language}.yml":
+                continue
+
+            filepath = os.path.join(root, filename)
+            with open(filepath, 'r', encoding='utf-8-sig') as file:
+                doc = yaml.load(file, Loader=Loader)
+                localization.update(doc[f"l_{language}"])
+
+    return localization
+
+def generate_localization():
+    english_loc = get_all_localization('english')
+
+    for language in non_english_languages:
+        missing_keys = []
+
+        language_loc = get_all_localization(language)
+        missing_keys = [key for key in english_loc.keys() if key not in language_loc]
+
+        if missing_keys:
+            generated_dict = {f"l_{language}": {key: english_loc[key] for key in missing_keys}}
+
+            language_dir = os.path.join('.', 'localization', language)
+            pathlib.Path(language_dir).mkdir(parents=True, exist_ok=True)
+            filepath = os.path.join(language_dir, f"newsfeed_generated_l_{language}.yml")
+
+            with open(filepath, 'w', encoding='utf-8-sig') as file:
+                file.write(yaml.dump(generated_dict, Dumper=Dumper))
+
+generate_localization()
